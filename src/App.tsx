49e60167fc1b18faa5cd30d29fee8c732ff4c3ae
learnjavabh <learnmsbh@outlook.de>
2025-05-11 22:44:37 +0200

    refactoring and added callback reloadTodos to TodoAdd

--- src/App.tsx
+++ src/App.tsx
@@ -26,8 +26,8 @@ function App() {
     return (
     <>
       <Routes>
-          <Route path={"/"} element={<Todos todos={allTodos} getAllTodos={getAllData}/>}></Route>
-          <Route path={"/add"} element={<TodoAdd/>}></Route>
+          <Route path={"/"} element={<Todos todos={allTodos} reloadTodos={getAllData}/>}></Route>
+          <Route path={"/add"} element={<TodoAdd reloadTodos={getAllData}/>}></Route>
           <Route path={"/edit/:id"} element={<TodoEdit todos={allTodos} getAllTodos= {getAllData}/>}></Route>
           <Route path={"/:id"} element={<TodoDetails todos={allTodos}/>}></Route>
       </Routes>
